4a66e0aa2a51829f3de6550ed6b80ed2a7962ab0
Azedine Ouhadou <45069518+Xperaz@users.noreply.github.com>
2023-01-31 13:39:10 +0000

    add min and max methodes

--- main.cpp
+++ main.cpp
@@ -27,6 +27,28 @@ bool	search(bst_node *root, int data)
 		return (search(root->right, data));
 }
 
+int min(bst_node *root)
+{
+	bst_node *tmp = root;
+	if (root == NULL)
+		return (0);
+	while (tmp->left != NULL)
+		tmp = tmp->left;
+	return tmp->data;
+}
+
+int max(bst_node *root)
+{
+	// we can use also root it won't produce any problem
+
+	bst_node *tmp = root;
+	if (root == NULL)
+		return (0);
+	while (tmp->right != NULL)
+		tmp = tmp->right;
+	return tmp->data;
+}
+
 bst_node *insert(bst_node *root, int data)
 {
 	if (root == NULL)
@@ -45,7 +67,10 @@ int main()
 	root = insert(root, 15);
 	root = insert(root, 10);
 	root = insert(root, 20);
-	int idx = search(root, 110);
-	std::cout << idx << std::endl; 
+	root = insert(root, 100);
+	root = insert(root, 300);
+	root = insert(root, 5);
+	std::cout << "min of this bst is: " << min(root) << std::endl; 
+	std::cout << "max of this bst is: " << max(root) << std::endl; 
 	return 0;
 }
